86512cec17151b61089a8c8b28fc34ef1e2e0dea
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2021-01-21 15:01:50 -0300

    fix: remove unnecessary modules (#6)

--- boxes/poll.php
+++ boxes/poll.php
@@ -1,9 +1,4 @@
 <?php
-
-if(PAGE !== 'news') {
-	return;
-}
-
 $poll = $db->query('SELECT `id`, `question` FROM `z_polls` WHERE end > ' . time() . ' ORDER BY `end` LIMIT 1');
 if($poll->rowCount() > 0) {
 	$poll = $poll->fetch(PDO::FETCH_ASSOC);
